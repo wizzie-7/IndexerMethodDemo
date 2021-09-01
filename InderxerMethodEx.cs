b8e9bb0317e9c19c49b0be1ff495df82cfd9f053
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 20:28:30 +0530

    first commit

--- InderxerMethodEx.cs
+++ InderxerMethodEx.cs
@@ -4,13 +4,56 @@ namespace IndexerMethodDemo
 {
     public class IndexerNames
     {
-
+        private string[] namelist = new string[size];
+        static public int size = 10;
+        public IndexerNames()
+        {
+            for(int i=0;i<size;i++)
+            {
+                namelist[i] = "N/A";
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                string temp;
+                if(index>=0 && index <= size-1)
+                {
+                    temp = namelist[index];
+                }
+                else
+                {
+                    temp = " ";
+                }
+                return temp;
+            }
+            set
+            {
+                if (index >= 0 && index <= size - 1)
+                {
+                    namelist[index] = value;
+                }
+            }
+        }
     }
     class InderxerMethodEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IndexerNames names = new IndexerNames();
+            names[0] = "Zara";
+            names[1] = "Riz";
+            names[2] = "Nuha";
+            names[3] = "Asif";
+            names[4] = "Davinder";
+            names[5] = "Sunil";
+            names[6] = "Rubic";
+
+            for (int i = 0; i < IndexerNames.size; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
         }
     }
 }
