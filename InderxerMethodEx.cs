dc16372e8c23e76591c68b48396d7d7535c4fc14
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 20:20:52 +0530

    Add project files.

---
+++ InderxerMethodEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IndexerMethodDemo
+{
+    public class IndexerNames
+    {
+
+    }
+    class InderxerMethodEx
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
